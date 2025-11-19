5ca677d4cd321807b992408e57efa9117300d1fe
Nullin <133748859+reeksss@users.noreply.github.com>
2025-11-19 15:44:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(mySin(0.01));
+        System.out.println(Math.sin(0.01));
+    }
+    public static double mySin(double x) {
+        return x;
+    }
+}

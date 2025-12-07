f112b7a4c69cf3dd0de4b97499b20e6502bea96f
Nullin <133748859+reeksss@users.noreply.github.com>
2025-12-07 16:42:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(mySin(0.01));
-        System.out.println(Math.sin(0.01));
+        double c = 0.01;
+        double a = mySin(c);
+        double b = Math.sin(c);
+        double d = b-a;
+        System.out.printf("Библиотечная - "+ b);
+        System.out.println();
+        System.out.printf("Функция - "+ a);
+        System.out.println();
+        System.out.printf("Разница - "+ d);
     }
     public static double mySin(double x) {
         return x;

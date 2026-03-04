f5f925516e2db3407bd2f69161dcfef2e001481f
Nullin <133748859+reeksss@users.noreply.github.com>
2026-03-04 15:32:36 +0300

    Add files via upload

---
+++ simply not optimized english trainer tgbot/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.example.MyBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+
+            botsApi.registerBot(new MyBot());
+
+            System.out.println("Бот успешно запущен и ждет сообщений!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
